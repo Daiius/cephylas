272278ed119fc02d46d08568a4447671b02317b5
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-11-21 17:37:14 +0900

    add disk information acquisition using /proc/diskstats

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,25 @@ use mem_info::get_mem_info;
 mod cpu_info;
 mod net_info;
 mod mem_info;
+mod disk_info;
 
 enum ApplicationError {
     CpuInfoError(cpu_info::CpuInfoError),
     NetInfoError(net_info::NetInfoError),
     MemInfoError(mem_info::MemInfoError),
+    DiskInfoError(disk_info::DiskInfoError),
 }
 impl std::fmt::Display for ApplicationError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ApplicationError::CpuInfoError(e) => write!(f, "CpuInfoError, {}", e),
-            ApplicationError::NetInfoError(e) => write!(f, "NetInfoError, {}", e),
-            ApplicationError::MemInfoError(e) => write!(f, "MemInfoError, {}", e),
+            ApplicationError::CpuInfoError(e) => 
+                write!(f, "CpuInfoError, {}", e),
+            ApplicationError::NetInfoError(e) => 
+                write!(f, "NetInfoError, {}", e),
+            ApplicationError::MemInfoError(e) => 
+                write!(f, "MemInfoError, {}", e),
+            ApplicationError::DiskInfoError(e) =>
+                write!(f, "DiskInfoError, {}", e),
         }
     }
 }
@@ -25,7 +32,6 @@ impl std::fmt::Debug for ApplicationError {
         write!(f, "{}", self.to_string())
     }
 }
-
 impl From<cpu_info::CpuInfoError> for ApplicationError {
     fn from(value: cpu_info::CpuInfoError) -> Self {
         ApplicationError::CpuInfoError(value)
@@ -40,6 +46,11 @@ impl From<mem_info::MemInfoError> for ApplicationError {
     fn from(value: mem_info::MemInfoError) -> Self {
         ApplicationError::MemInfoError(value)
     }
+}
+impl From<disk_info::DiskInfoError> for ApplicationError {
+   fn from(value: disk_info::DiskInfoError) -> Self {
+       ApplicationError::DiskInfoError(value)
+   }
 }
 
 fn run_threads(n: u8) {
@@ -69,9 +80,11 @@ fn main() -> Result<(), ApplicationError> {
 
     let cpu_info_first = cpu_info::get_cpu_info()?;
     let net_info_first = net_info::get_net_info("eth0")?;
+    let disk_info_first = disk_info::get_disk_info("sdb")?;
     std::thread::sleep(std::time::Duration::from_secs(1));
     let cpu_info_second = cpu_info::get_cpu_info()?;
     let net_info_second = net_info::get_net_info("eth0")?;
+    let disk_info_second = disk_info::get_disk_info("sdb")?;
 
     let diff_cpu = cpu_info_second - cpu_info_first;
     println!(
@@ -83,6 +96,12 @@ fn main() -> Result<(), ApplicationError> {
         "net usage: RX {} bytes/s, TX {} bytes/s", 
         diff_net.rx.bytes, 
         diff_net.tx.bytes
+    );
+    let diff_disk = disk_info_second - disk_info_first;
+    println!(
+        "disk usage: R/W  {}/{} bytes",
+        diff_disk.reads_completed * disk_info::SECTOR_SIZE,
+        diff_disk.writes_completed * disk_info::SECTOR_SIZE,
     );
 
     let mem_info = get_mem_info()?;
