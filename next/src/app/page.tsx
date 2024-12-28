2e937e6a4e347ba05f5bdd49f5ca101f73b5aca9
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-12-28 15:14:54 +0900

    add net and disk graphs

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -3,22 +3,51 @@ export const revalidate = 10;
 
 
 import Chart from '@/components/Chart';
+import {  } from 'chart.js/auto'
 import { readLogs } from '@/lib/logReader';
 
 
 export default async function Home() {
+
   const logs = await readLogs();
+
+  // could not find exported one,
+  // so i copied it from Chart.js source code.
+  const borderColors = [
+    'rgb(54, 162, 235)', // blue
+    'rgb(255, 99, 132)', // red
+    'rgb(255, 159, 64)', // orange
+    'rgb(255, 205, 86)', // yellow
+    'rgb(75, 192, 192)', // green
+    'rgb(153, 102, 255)', // purple
+    'rgb(201, 203, 207)' // grey
+  ];
+  const backgroundColors = borderColors
+    .map(bc => bc.replace('rgb(', 'rgba(').replace(')', ',0.5)'));
   
   const datasetsCpu = Object.entries(logs)
-    .map(([key, value]) => ({
-      label: key, data: value,
-      parsing: { xAxisKey: 'time', yAxisKey: 'cpu.percentage', },
+    .map(([label, data]) => ({
+      label, data, parsing: { xAxisKey: 'time', yAxisKey: 'cpu.percentage', },
     }));
   const datasetsMemory = Object.entries(logs)
-    .map(([key, value]) => ({
-      label: key, data: value,
-      parsing: { xAxisKey: 'time', yAxisKey: 'memory.percentage', },
+    .map(([label, data]) => ({
+      label, data, parsing: { xAxisKey: 'time', yAxisKey: 'memory.percentage', },
     }));
+  const datasetsDisk = Object.entries(logs)
+    .flatMap(([label, data], ilabel) => ["read", "write"].map(type => ({
+      label: `${label} ${type}`, data, parsing: { xAxisKey: 'time', yAxisKey: `io.${type}kBps` },
+      borderColor: borderColors[ilabel & borderColors.length],
+      backgroundColor: backgroundColors[ilabel % backgroundColors.length],
+      borderDash: type === "read" ? [5,5] : [1,0],
+    })));
+  const datasetsNet = Object.entries(logs)
+    .flatMap(([label, data], ilabel) => ["send", "recv"].map(type => ({
+      label: `${label} ${type}`, data, parsing: { xAxisKey: 'time', yAxisKey: `net.${type}kBps` },
+      borderColor: borderColors[ilabel % borderColors.length],
+      backgroundColor: backgroundColors[ilabel % backgroundColors.length],
+      borderDash: type === "send" ? [5,5] : [1,0],
+    })));
+
 
   return (
     <div>
@@ -29,6 +58,14 @@ export default async function Home() {
       <Chart 
         chartId='chartjs-memory-usage'
         datasets={datasetsMemory} title='Memory usage (%)'
+      />
+      <Chart
+        chartId='chartjs-disk-usage'
+        datasets={datasetsDisk} title='Disk usage (kB/s)'
+      />
+      <Chart
+        chartId='chartjs-net-usage'
+        datasets={datasetsNet} title='Net usage (kB/s)'
       />
     </div>
   );
